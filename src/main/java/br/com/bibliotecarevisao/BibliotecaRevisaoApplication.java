a6867e25fd56366a9c8606eb3a8333d578b021f7
Renato Aires <rentonaires@gmail.com>
2023-05-16 20:04:59 -0300

    first commit

---
+++ src/main/java/br/com/bibliotecarevisao/BibliotecaRevisaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.bibliotecarevisao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaRevisaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaRevisaoApplication.class, args);
+	}
+
+}
